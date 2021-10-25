1260e9b9e32f448758bc772b68f5904685e45807
shahjalal <shahjalalkhan215@gmail.com>
2021-10-25 20:42:30 +0600

    mouse move javascript

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const btn = document.querySelector('.btn')
-btn.addEventListener('mousemove', (e) => {
-   const x = e.pageX - btn.offsetLeft
-   const y = e.pageY - btn.offsetTop
-
-   btn.style.setProperty('--x', x + 'px')
-   btn.style.setProperty('--y', y + 'px')
+const slide = document.querySelector('#slide')
+window.addEventListener('mousemove', (e) => {
+    var x = e.clientX
+    slide.style.left = x +'px'
+   
 })
+
